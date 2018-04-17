ffa7eb3bf76e2ffb813a51da4f17e71db09910c6
webmobikdm <38469394+webmobikdm@users.noreply.github.com>
2018-04-17 18:34:09 +0300

    Add files via upload

---
+++ Contact-form/contact-form/contact-form.php
@@ -0,0 +1,40 @@
+﻿<?php
+
+/* Задаем переменные */
+$name = htmlspecialchars($_POST["name"]);
+$email = htmlspecialchars($_POST["email"]);
+$tel = htmlspecialchars($_POST["tel"]);
+//$website = htmlspecialchars($_POST["website"]);
+$message = htmlspecialchars($_POST["message"]);
+$bezspama = htmlspecialchars($_POST["bezspama"]);
+
+/* Ваш адрес и тема сообщения */
+$address = "ddimon2000@bk.ru.com";
+$sub = "Сообщение с сайта Ipad, Iphone product";
+
+/* Формат письма */
+$mes = "Сообщение с сайта Ipad, Iphone product.\n
+Имя отправителя: $name 
+Электронный адрес отправителя: $email
+Телефон отправителя: $tel
+Текст сообщения:
+$message";
+
+
+if (empty($bezspama)) /* Оценка поля bezspama - должно быть пустым*/
+{
+/* Отправляем сообщение, используя mail() функцию */
+$from = "Reply-To: $email \r\n";
+if (mail($address, $sub, $mes, $from)) {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо отправлено, через 5 секунд вы вернетесь на страницу Ipad, Iphone product</body>';}
+else {
+	header('Refresh: 5; URL=http://biznessystem.ru');
+	echo '<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>
+    <body>Письмо не отправлено, через 5 секунд вы вернетесь на страницу Ipad, Iphone product</body>';}
+}
+exit; /* Выход без сообщения, если поле bezspama чем-то заполнено */
+?>
